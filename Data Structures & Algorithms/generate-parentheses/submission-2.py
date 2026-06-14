adf25347a728e086ee5e5e9d887f636c3d958703
Arnav Dev <arnav.dev2006@gmail.com>
2026-06-14 22:23:01 +0530

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def sol(ind, total, subsetstr):
+            if ind >= 2 * n:
+                if total == 0:
+                    res.append(subsetstr)
+                return
+            if total > n :
+                return
+            elif total < 0:
+                return 
+            
+            sol(ind +1, total + 1, subsetstr + "(")
+            sol(ind+1, total -1, subsetstr + ")" )
+        
+        sol(0,0,"")
+        return res
+                
