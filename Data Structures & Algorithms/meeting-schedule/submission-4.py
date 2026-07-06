e24aa1dd7e4e0866270879956658ee594ee18515
Arnav Dev <arnav.dev2006@gmail.com>
2026-07-06 14:17:19 +0530

    Add: meeting-schedule - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule/submission-4.py
@@ -0,0 +1,23 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        n = len(intervals)
+        if n == 0 :
+            return True
+        intervals.sort(key = lambda x: x.end)
+
+        lastend = intervals[0].end
+        cnt = 1
+
+        for i in range(1,n):
+            if intervals[i].start < lastend :
+                return False
+            lastend = intervals[i].end
+        return True
